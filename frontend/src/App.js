6296b30c24aa5d872389eeea9aad2b4fe78c5ed5
jas112 <jas.sandmaster@gmail.com>
2022-06-23 17:46:54 -0700

    added frontend routing/routes and modified Header.jsx

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
-import './App.css';
+import { Browser as Router, Routes, Route } from 'react-router-dom'
+import {ToastContainer} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css' 
+import Dashboard from './views/Dashboard';
+import Register from './views/Register';
+import Login from './views/Login';
+import Header from './components/Header';
+
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <>
+      <Router>
+        <div className='container'>
+          <Header/>
+          <Routes>
+            <Route path='/' element={<Dashboard/>} />
+            <Route path='/register' element={<Register/>} />
+            <Route path='/login' element={<Login/>} />
+          </Routes>
+        </div>
+        <ToastContainer/>
+      </Router>
+    </>
   );
 }
 
